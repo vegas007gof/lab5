6db6764c130d8ee6f9f47fb258dc4b525e65c1d0
vegas007gof <vegas007gof@mail.ru>
2022-02-24 23:12:23 +0300

    dd

--- pyProj/individual.py
+++ pyProj/individual.py
@@ -1,7 +1,11 @@
-N = int(input("N "))
-if N == 1:
-    print(f"я купил {N} карандаш")
-elif N < 5:
-    print(f"я купил {N} карандаша")
-elif N < 11:
-    print(f"я купил {N} карандашей")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    N = int(input("N "))
+    if N == 1:
+        print(f"я купил {N} карандаш")
+    elif N < 5:
+        print(f"я купил {N} карандаша")
+    elif N < 11:
+        print(f"я купил {N} карандашей")
